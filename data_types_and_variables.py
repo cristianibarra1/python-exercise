4383fb8596a3c8f3bf9da8b1796eed5027bdae93
cristian <107886623+cristianibarra1@users.noreply.github.com>
2022-07-12 14:34:11 -0500

    Create data_types_and_variables.py

---
+++ data_types_and_variables.py
@@ -0,0 +1,87 @@
+99.9
+
+#"False"
+
+False
+
+#'0'
+
+0
+#
+True
+#
+'True'
+#
+[{}]
+#
+{'a': []}
+type('1' + 2)
+#type 3
+type 6 % 4
+#int 2
+type(6 % 4)
+#int 
+type(type(6 % 4))
+#type 2
+type('3 + 4 is ' + 3 + 4)
+#int 7
+type(0 < 0)
+#bool
+type('False' == False)
+#bool false
+type(True == 'True')
+#vool true
+type(5 >= -5)
+#bool true
+type(True or "42")
+#bool true
+type(6 % 5)
+#int 1
+type(5 < 4 and 1 == 1)
+#bool false
+type('codeup' == 'codeup' and 'codeup' == 'Codeup')
+#bool true
+type(4 >= 0 and 1 ! == '1')
+#int false
+type(6 % 3 == 0)
+#bool true
+type(5 % 2 != 0)
+#bool true
+type([1] + 2)
+#int ([1]+2
+type([1] + [2])
+#list [1, 2]
+type([1] * 2)
+#list 1, 1]
+type([1] * [2])
+#list
+type([] + [] == [])
+#list
+type{} +
+# list 
+# ***You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?***
+little= 3
+brother=5
+hercules=1
+(brother*3)+(little*3)+(hercules*3)
+# 27
+#Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+google=400
+amzon=380
+facebook=350
+(google*6)+(facebook*10)+amzon*4 #7420
+#A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+
+
+#A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+
+
+#Create a variable that holds a boolean value for each of the following conditions:
+#***the password must be at least 5 characters
+#the username must be no more than 20 characters
+#the password must not be the same as the username
+#bonus neither the username or password can start or end with whitespace***
+username='popgoes'
+password='fun3030'
+username=='popgoes' and password=='fun3030'
